58a0cbaf8c83795f79845e49a496403d766a9033
Kasia Surzyn <k.n.surzyn@gmail.com>
2020-01-15 03:19:50 +0100

    Add searchig relation method and method to Graph

--- dependencyFinders/ModuleDependencies.py
+++ dependencyFinders/ModuleDependencies.py
@@ -18,3 +18,31 @@ class ModuleDependencies:
                     # potrzebuje całej ścieżki do pliku
                     file_names.append(name_of_curent_directory + '/' + file_name)
         return file_names
+
+    def get_relation_names(self):
+        list_nodes = ModuleDependencies().get_directory_names()
+        tmp = []
+        # iteruje po folderach
+        for current_node in list_nodes:
+            current_filenames_in_directory_list = ModuleDependencies().get_current_filenames_in_directory(current_node)
+            # iteruje po plikach w folderze
+            for current_file in current_filenames_in_directory_list:
+                if os.path.isfile(current_file):
+                    with open(current_file) as file:
+                        for line in file:
+                            line = line.strip()
+                            if line.startswith("from "):
+                                file_package = line.split(' ')[1].split('.')[0]  # wycinam nazwę modułu z linii
+                                if file_package in list_nodes:
+                                    if file_package == current_node:
+                                        continue
+                                    tmp.append(tuple((current_node, file_package)))
+                                else:
+                                    continue
+                            #
+                            elif line.startswith('def'):
+                                tmp.append(tuple((line.split(' ')[1].split('(')[0], current_file.split('/')[0])))
+        tmp2 = []
+        for i in tmp:
+            tmp2.append(tuple((tmp.count(i), i)))
+        return list(set([i for i in tmp2]))
